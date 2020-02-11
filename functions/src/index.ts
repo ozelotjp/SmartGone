cb415497198f2b325b6ae8af2a7c1a4cec7e8057
ozelotjp <contact@ozelot.app>
2020-02-12 04:09:51 +0900

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,62 @@
 /* eslint no-console: off */
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+const server = functions.region('asia-northeast1')
 admin.initializeApp()
 
-exports.onTouch = functions.https.onRequest((request, response) => {
+exports.onTouch = server.https.onRequest((request, response) => {
   if (
-    typeof request.query.idm !== 'undefined' &&
-    typeof request.query.location !== 'undefined' &&
-    typeof request.query.type !== 'undefined'
+    typeof request.query.idm === 'undefined' ||
+    typeof request.query.location === 'undefined' ||
+    typeof request.query.type === 'undefined'
   ) {
-    admin
-      .firestore()
-      .collection('cards')
-      .doc(request.query.idm)
-      .get()
-      .then((snapshot) => {
-        admin
-          .firestore()
-          .collection('check')
-          .doc(snapshot.data().user)
-          .set({
-            date: admin.firestore.FieldValue.serverTimestamp,
-            location: request.query.location,
-            type: request.query.type
-          })
-          .catch((error) => {
-            console.error(error)
-          })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+    response.json({
+      status: 'error',
+      description: 'no query'
+    })
+    return
   }
-  response.send('done')
+
+  admin
+    .firestore()
+    .collection('cards')
+    .doc(request.query.idm)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists === false) {
+        response.json({
+          status: 'error',
+          description: 'no snapshot'
+        })
+        return
+      }
+      admin
+        .firestore()
+        .collection('check')
+        .doc(snapshot.data().user)
+        .set({
+          date: admin.firestore.FieldValue.serverTimestamp(),
+          location: request.query.location,
+          type: request.query.type
+        })
+        .then(() => {
+          response.json({
+            status: 'success'
+          })
+        })
+        .catch((error) => {
+          response.json({
+            status: 'exception',
+            description: 'store:check',
+            detailes: error
+          })
+        })
+    })
+    .catch((error) => {
+      response.json({
+        status: 'exception',
+        description: 'store:cards',
+        detailes: error
+      })
+    })
 })
